cb824063b68397690f48687da70db3fdb292d973
Aaron <aaron@sapientacademy.com>
2026-04-13 16:03:33 -0700

    updated proxy

--- proxy.js
+++ proxy.js
@@ -9,5 +9,8 @@ export default clerkMiddleware(async (auth, request) => {
 }, { signInUrl: '/sign-in' })
 
 export const config = {
-  matcher: ['/((?!_next|api|.*\\..*).*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 }
